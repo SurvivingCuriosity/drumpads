cd71f5c1b5ce27295d8ccbee5ee5ab2f2cf0486e
Fernando Rodríguez <ferrodest1999@gmail.com>
2024-07-20 13:33:56 +0200

    Change build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,52 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { VitePWA } from "vite-plugin-pwa";
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
+import { visualizer } from 'rollup-plugin-visualizer';
+
 export default defineConfig({
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
+          if (id.includes('node_modules')) {
+            if (id.includes('react-select')) {
+              return 'react-select';
+            }
+            if (id.includes('@dnd-kit/core')) {
+              return 'dnd-kit-core';
+            }
+            return 'vendor';
           }
-          if (id.includes("/src/components/")) {
-            return "components";
+          if (id.includes('/src/components/')) {
+            return 'components';
           }
-          // Puedes agregar más reglas aquí si lo necesitas
         },
       },
     },
-    chunkSizeWarningLimit: 500, // ajusta este valor según tus necesidades
   },
   plugins: [
     react(),
     VitePWA({
       manifest: {
-        name: "KrumDit",
-        short_name: "KrumDit",
+        name: 'KrumDit',
+        short_name: 'KrumDit',
         icons: [
           {
-            src: "icons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: 'icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "icons/android-chrome-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
+            src: 'icons/android-chrome-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
           },
         ],
-        theme_color: "#ffffff",
-        background_color: "#181818",
-        display: "standalone",
+        theme_color: '#ffffff',
+        background_color: '#181818',
+        display: 'standalone',
       },
     }),
+    visualizer({ open: true }),
   ],
 });
