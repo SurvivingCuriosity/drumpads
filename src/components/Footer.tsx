141e58049e1687c68b6120ca54f0025e4790dad8
Fernando Rodríguez <ferrodest1999@gmail.com>
2024-07-20 12:41:51 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,11 @@ import { ThemePicker } from './ThemePicker';
 export const Footer = () => {
     return (
 
-
         <footer className="bg-neutral-900 shadow">
             <div className="mx-auto w-full max-w-screen-xl p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <div className="mb-4 flex items-center space-x-3 sm:mb-0 rtl:space-x-reverse">
-                        <img src={logo} className="h-8" alt="KrumDit Logo" />
+                        <img src={logo} alt="KrumDit Logo" style={{width: 32, height: 32}} />
                         <span className="self-center whitespace-nowrap text-2xl font-semibold text-white">KrumDit</span>
                         <p className='text-neutral-400'>by <a target="_blank" href='https://github.com/SurvivingCuriosity' className='hover:underline'>SurvivingCuriosity</a></p>
                     </div>
